c40343a0b34da00bb2ef52e818e80784205a8e9c
Qstimo <vadtolpekin@gmail.com>
2023-09-16 17:09:28 +0300

    fetch/redux2

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,74 +1,87 @@
 import React from 'react';
 import s from './Home.module.scss';
-import axios from 'axios';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectSearch } from '../../redux/slices/filterSlice';
 import VideoCard from '../../Component/videiosCard';
 import SkeletonVideoCard from '../../Component/videiosCard/SkeletonVideoCard';
+import { fetchVideos, selectVideosData } from '../../redux/slices/videosSlice';
 
 function Home() {
+  const dispatch = useDispatch();
+  const { items, status } = useSelector(selectVideosData);
   const [videos, setVideos] = React.useState([]);
 
+  const getDataVideos = async () => {
+    const query = search ? search : '';
+    const regionCode = 'RU'; // Код региона, например, RU для России
+    const order = 'relevance'; // Порядок сортировки, например, relevance или date
+    const maxResults = 12; // Максимальное количество результатов поиска
+    dispatch(fetchVideos({ query, regionCode, order, maxResults }));
+  };
+
   const search = useSelector(selectSearch);
+  // React.useEffect(() => {
+  //   // const dataGet = async () => {
+  //   //   const response = await axios.get(
+  //   //     `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%20&chart=mostPopular&maxResults=30&regionCode=ru&key=AIzaSyD0r7qUDnou4e9FX-_x2h503aErHcX4wn4`,
+  //   //   );
+
+  //   //   setVideos(response.data.items);
+  //   // };
+  //   // async function searchVideos(query) {
+  //   //   const apiKey = 'AIzaSyD0r7qUDnou4e9FX-_x2h503aErHcX4wn4';
+  //   //   // const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${apiKey}`;
+  //   //   const apiUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%20&chart=mostPopular&maxResults=30&regionCode=ru&q=${query}&key=${apiKey}`;
+
+  //   //   try {
+  //   //     const response = await axios.get(apiUrl);
+
+  //   //      setVideos(response.data.items);
+  //   //   } catch (error) {
+  //   //     console.error(error);
+
+  //   //   }
+  //   // }
+
+  //   async function searchVideos(query, regionCode, order, maxResults) {
+  //     const apiKey = 'AIzaSyD0r7qUDnou4e9FX-_x2h503aErHcX4wn4';
+  //     const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&regionCode=${regionCode}&order=${order}&maxResults=${maxResults}&key=${apiKey}`;
+
+  //     try {
+  //       const response = await axios.get(apiUrl);
+  //       return response.data;
+  //     } catch (error) {
+  //       console.error(error);
+  //       return null;
+  //     }
+  //   }
+
+  //   const query = 'котики';
+  //   const regionCode = 'RU'; // Код региона, например, RU для России
+  //   const order = 'relevance'; // Порядок сортировки, например, relevance или date
+  //   const maxResults = 12; // Максимальное количество результатов поиска
+
+  //   searchVideos(search, regionCode, order, maxResults)
+  //     .then((data) => {
+  //       setVideos(data.items);
+  //       console.log(data);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }, [search]);
+
   React.useEffect(() => {
-    // const dataGet = async () => {
-    //   const response = await axios.get(
-    //     `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%20&chart=mostPopular&maxResults=30&regionCode=ru&key=AIzaSyD0r7qUDnou4e9FX-_x2h503aErHcX4wn4`,
-    //   );
-
-    //   setVideos(response.data.items);
-    // };
-    // async function searchVideos(query) {
-    //   const apiKey = 'AIzaSyD0r7qUDnou4e9FX-_x2h503aErHcX4wn4';
-    //   // const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${apiKey}`;
-    //   const apiUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%20&chart=mostPopular&maxResults=30&regionCode=ru&q=${query}&key=${apiKey}`;
-    
-    //   try {
-    //     const response = await axios.get(apiUrl);
-      
-    //      setVideos(response.data.items);
-    //   } catch (error) {
-    //     console.error(error);
-        
-    //   }
-    // }
-
-    async function searchVideos(query, regionCode, order, maxResults) {
-      const apiKey = 'AIzaSyD0r7qUDnou4e9FX-_x2h503aErHcX4wn4';
-      const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&regionCode=${regionCode}&order=${order}&maxResults=${maxResults}&key=${apiKey}`;
-    
-      try {
-        const response = await axios.get(apiUrl);
-        return response.data;
-      } catch (error) {
-        console.error(error);
-        return null;
-      }
-  
-    }
-
-const query = 'котики';
-const regionCode = 'RU'; // Код региона, например, RU для России
-const order = 'relevance'; // Порядок сортировки, например, relevance или date
-const maxResults = 12; // Максимальное количество результатов поиска
-
-searchVideos(search, regionCode, order, maxResults)
-  .then((data) => {
-    setVideos(data.items);
-    console.log(data)
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-
+    getDataVideos();
   }, [search]);
-  const skeleton = [...new Array(12)].map(i=><SkeletonVideoCard key={i}/>);
 
-
+  const skeleton = [...new Array(12)].map((i) => <SkeletonVideoCard key={i} />);
+  if (status === 'error') {
+    return <h1>error</h1>;
+  }
   return (
     <div className={s.content}>
-     {videos ? skeleton: <VideoCard videos={videos}/>}
-      
+      {status === 'loading' ? skeleton : <VideoCard videos={items} />}
     </div>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from '../Component/Theme/slice'
 import filter from './slices/filterSlice'
+import videos from "./slices/videosSlice";
 
 const store = configureStore({
     reducer: {
         theme: themeReducer,
         filter,
+        videos,
+
     }
 })
 
