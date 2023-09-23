5581e1977f0ce16c00e628bdde28c3bcbf2c7177
Qstimo <vadtolpekin@gmail.com>
2023-09-23 13:47:30 +0300

    input

--- src/Component/Ui/MyInput/MyInput.tsx
+++ src/Component/Ui/MyInput/MyInput.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import s from './MyInput.module.scss';
 
-type MyInputProps={
-
+type MyInputProps = {
+  value: string,
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  removeInput: () => void;
 }
 
-const MyInput:React.FC<any>= ({ ...props }) => {
+const MyInput: React.FC<MyInputProps> = ({ value, onChange, removeInput }) => {
+
   return (
     <form className={s.form}>
-      <input  {...props} />
+      <input value={value} type="text" placeholder="Искать здесь..." onChange={onChange} />
       <button type="submit"></button>
       <div className={s.icon}>
         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -43,6 +46,10 @@ const MyInput:React.FC<any>= ({ ...props }) => {
           </g>
         </svg>
       </div>
+      <div className={value ? s.clear : s.none} onClick={removeInput}>
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9 9L15 15" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M15 9L9 15" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <circle cx="12" cy="12" r="9" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></circle> </g></svg>
+      </div>
+
     </form>
   );
 };
