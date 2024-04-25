4ba8b60613b9e43acce88de4ba9b5c794fe98705
Vadim Tolpekin <vadtolpekin@gmail.com>
2024-04-25 19:35:09 +0300

    fix menu page

--- src/plugins/AutoReload/versionGenerator.js
+++ src/plugins/AutoReload/versionGenerator.js
@@ -1,20 +1,5 @@
 const fs = require('fs-extra');
 
-// module.exports = {
-
-//     generateVersionFile: function (filename) {
-//         const packageJson = fs.readFileSync('./package.json');
-//         const version = JSON.parse(packageJson).version || 0;
-
-//         fs.writeFileSync(filename, `{
-//     "AppVersion": "${version}",
-//     "Build": "${process.env.NODE_ENV}",
-//     "BundleVersion": "${new Date().toISOString()}"
-// }
-// `);
-//     }
-// }
-
 function generateVersionFile(filePath) {
     const packageJson = fs.readFileSync('./package.json');
     const version = JSON.parse(packageJson).version || 0;
